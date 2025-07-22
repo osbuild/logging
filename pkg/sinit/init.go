512a951ac276d93acd6ba281628f91c1ab008a69
Lukas Zapletal <lzap+git@redhat.com>
2025-07-22 13:20:33 +0200

    splunk: set hostname if not configured
    Splunk configuration now defaults to the system hostname if not
    explicitly set. This is just to avoid calling `os.Hostname()` in every
    project that uses this package or when hostname was provided.

--- pkg/sinit/init.go
+++ pkg/sinit/init.go
@@ -163,6 +163,8 @@ var (
 	ErrSentryInitialization = errors.New("sentry initialization error")
 )
 
+var osHostname = os.Hostname
+
 // InitializeLogging initializes the logging system with the provided
 // configuration. Use Close to ensure all logs are written before exiting.
 // Subsequent calls to InitializeLogging will lead to ErrAlreadyInitialized.
@@ -213,6 +215,15 @@ func InitializeLogging(ctx context.Context, config LoggingConfig) error {
 	}
 
 	if config.SplunkConfig.Enabled {
+		if config.SplunkConfig.Hostname == "" {
+			hostname, err := osHostname()
+			if err != nil {
+				return fmt.Errorf("failed to get hostname: %w", err)
+			}
+
+			config.SplunkConfig.Hostname = hostname
+		}
+
 		c := splunk.SplunkConfig{
 			Level:    parseLevel(config.SplunkConfig.Level),
 			URL:      config.SplunkConfig.URL,
