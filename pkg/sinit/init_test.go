3a80b1b3b2fa992cbb56f34112992109846f9615
Lukas Zapletal <lzap+git@redhat.com>
2025-07-22 13:20:33 +0200

    splunk: set hostname if not configured
    Splunk configuration now defaults to the system hostname if not
    explicitly set. This is just to avoid calling `os.Hostname()` in every
    project that uses this package or when hostname was provided.

--- pkg/sinit/init_test.go
+++ pkg/sinit/init_test.go
@@ -3,9 +3,20 @@ package sinit
 import (
 	"context"
 	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 )
+
+func init() {
+	osHostname = func() (string, error) {
+		return "default-hostname", nil
+	}
+}
 
 func TestValidationEmpty(t *testing.T) {
 	cfg := LoggingConfig{}
@@ -65,5 +76,94 @@ func TestValidationSplunkEmptyURL(t *testing.T) {
 
 	if err := validate(cfg); !errors.Is(err, ErrMissingURL) {
 		t.Fatalf("expected ErrMissingURL error, got %v", err)
+	}
+}
+
+func TestValidationSplunkWithHostname(t *testing.T) {
+	ch := make(chan string)
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "failed to read body", http.StatusInternalServerError)
+			return
+		}
+		ch <- string(body)
+	}))
+	defer srv.Close()
+
+	ctx := context.Background()
+	cfg := LoggingConfig{
+		SplunkConfig: SplunkConfig{
+			Enabled:  true,
+			URL:      srv.URL,
+			Hostname: "some-test-hostname",
+		},
+	}
+
+	err := InitializeLogging(ctx, cfg)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	defer func() {
+		if err := Close(100 * time.Millisecond); err != nil {
+			t.Fatalf("expected no error on close, got %v", err)
+		}
+	}()
+
+	slog.Warn("foo")
+	Flush()
+
+	select {
+	case splunkBody := <-ch:
+		if !strings.Contains(splunkBody, `"host":"some-test-hostname"`) {
+			t.Fatalf("expected hostname to be set in splunk body, got %s", splunkBody)
+		}
+	case <-time.After(6 * time.Second):
+		panic("no splunk record in 6s")
+	}
+}
+
+func TestValidationSplunkWithoutHostname(t *testing.T) {
+	ch := make(chan string)
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "failed to read body", http.StatusInternalServerError)
+			return
+		}
+		ch <- string(body)
+	}))
+	defer srv.Close()
+
+	ctx := context.Background()
+	cfg := LoggingConfig{
+		SplunkConfig: SplunkConfig{
+			Enabled: true,
+			URL:     srv.URL,
+		},
+	}
+
+	err := InitializeLogging(ctx, cfg)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	defer func() {
+		if err := Close(100 * time.Millisecond); err != nil {
+			t.Fatalf("expected no error on close, got %v", err)
+		}
+	}()
+
+	slog.Warn("foo")
+	Flush()
+
+	select {
+	case splunkBody := <-ch:
+		if !strings.Contains(splunkBody, `"host":"default-hostname"`) {
+			t.Fatalf("expected hostname to be set in splunk body, got %s", splunkBody)
+		}
+	case <-time.After(6 * time.Second):
+		panic("no splunk record in 6s")
 	}
 }
