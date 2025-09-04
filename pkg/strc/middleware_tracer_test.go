3d74189588b25e6c051eaad7bb6468b4116158fb
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-09-04 16:05:50 +0200

    strc/middleware: add simplified echo middleware
    Simplifies the logging middleware for echo. It takes the following actions:
    - times the request;
    - adds an error in case the status is 5xx.
    
    Also splits out the tracing middleware. Which adds a trace id and span
    id to the request context.
    
    The `EchoContextSetLogger` can be used to set the logger in the echo
    context.

---
+++ pkg/strc/middleware_tracer_test.go
@@ -0,0 +1,34 @@
+package strc_test
+
+import (
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/logging/pkg/collect"
+	"github.com/osbuild/logging/pkg/strc"
+)
+
+func TestEchoTracer(t *testing.T) {
+	logHandler := collect.NewTestHandler(slog.LevelDebug, false, false, false)
+	logger := slog.New(logHandler)
+	e := echo.New()
+
+	e.Use(strc.EchoTracer(strc.MiddlewareConfig{}))
+	e.Use(strc.EchoRequestLogger(logger, strc.MiddlewareConfig{}))
+
+	var traceID string
+	e.GET("/ok", func(c echo.Context) error {
+		traceID = strc.TraceIDFromContext(c.Request().Context()).String()
+		return c.String(http.StatusOK, "OK")
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "http://example.com/ok", nil)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(rec, req)
+	assert.True(t, logHandler.Contains(traceID, strc.TraceIDKey))
+}
