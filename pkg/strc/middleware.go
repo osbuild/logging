a23522fc65e51112af21d7fa5f02ae465e5821c1
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-09-05 11:49:23 +0200

    strc/middleware: remove unused config options
    These config options don't do anything (anymore), let's remove them and
    add them when support is added.

--- pkg/strc/middleware.go
+++ pkg/strc/middleware.go
@@ -35,31 +35,4 @@ type MiddlewareConfig struct {
 
 	// ServerErrorLevel is the log level for requests with server errors (5xx). Defaults to Error.
 	ServerErrorLevel slog.Level
-
-	// SpanName is the name of the span. Defaults to "http request".
-	SpanName string
-
-	// WithUserAgent enables logging of the User-Agent header. Defaults to false.
-	WithUserAgent bool
-
-	// WithRequestBody enables logging of the request body. Defaults to false.
-	WithRequestBody bool
-
-	// WithRequestHeader enables logging of the request headers. Defaults to false.
-	WithRequestHeader bool
-
-	// WithResponseBody enables logging of the response body. Defaults to false.
-	WithResponseBody bool
-
-	// WithResponseHeader enables logging of the response headers. Defaults to false.
-	WithResponseHeader bool
-
-	// WithSpanID enables logging of the span ID. Defaults to false.
-	WithTraceID bool
-
-	// WithSpanID enables logging of the span ID. Defaults to false.
-	WithSpanID bool
-
-	// NoExtractTraceID disables extracting trace id from incoming requests. Defaults to false.
-	NoTraceContext bool
 }
