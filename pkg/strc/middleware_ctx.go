f8c3b5aabd957c0ea958ef50060489a94e445d27
Lukas Zapletal <lzap+git@redhat.com>
2025-09-08 17:26:30 +0200

    strc: convert HeadersExtractor to echo

--- pkg/strc/middleware_ctx.go
+++ pkg/strc/middleware_ctx.go
@@ -3,32 +3,35 @@ package strc
 import (
 	"context"
 	"log/slog"
-	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-// HeadfieldPair is a pair of header name and field name.
-type HeadfieldPair struct {
+// HeaderField is a pair of header name and field name.
+type HeaderField struct {
 	HeaderName string
 	FieldName  string
 }
 
-// HeadfieldPairMiddleware is a middleware that extracts values from headers and stores them in the context.
-func HeadfieldPairMiddleware(pairs []HeadfieldPair) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// EchoHeadersExtractor is a middleware that extracts values from headers and
+// stores them in the context.
+func EchoHeadersExtractor(pairs []HeaderField) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
 			for _, p := range pairs {
-				if value := r.Header.Get(p.HeaderName); value != "" {
-					r = r.WithContext(context.WithValue(r.Context(), p, value))
+				if value := c.Request().Header.Get(p.HeaderName); value != "" {
+					c.SetRequest(c.Request().WithContext(context.WithValue(c.Request().Context(), p, value)))
 				}
 			}
 
-			next.ServeHTTP(w, r)
-		})
+			return next(c)
+		}
 	}
 }
 
-// HeadfieldPairCallback is a slog callback that extracts values from the context and adds them to the attributes.
-func HeadfieldPairCallback(pairs []HeadfieldPair) MultiCallback {
+// HeadersCallback is a slog callback that extracts values from the
+// context and adds them to the attributes.
+func HeadersCallback(pairs []HeaderField) MultiCallback {
 	return func(ctx context.Context, a []slog.Attr) ([]slog.Attr, error) {
 		for _, p := range pairs {
 			if value, ok := ctx.Value(p).(string); ok {
