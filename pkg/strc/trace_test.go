c705c323f4a1f67674451ab75ac96b5eac714390
Lukas Zapletal <lzap+git@redhat.com>
2025-09-23 17:10:36 +0200

    strc: add ability to override time in Span.Event and Span.End
    Span start, event and end time is automatically taken via `time.Now()`
    call but there are some use cases when this needs to be overridden to a
    specific time. Use special attributes to do that:
    
        span := strc.Start(ctx, "span name", "started", time.Now())
        span.Event("an event", "at", time.Now())
        span.End("finished", time.Now())
    
    Also adds TraceID() method to Span to return the trace ID. This will be
    usefil in images/ library to collect logs from JSONSeq osbuild monitor and
    keep them in a LRU cache.

--- pkg/strc/trace_test.go
+++ pkg/strc/trace_test.go
@@ -8,9 +8,11 @@ import (
 	"regexp"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestLogTextHandler(t *testing.T) {
+	t.Setenv("TZ", "UTC")
 	var buf bytes.Buffer
 
 	src = rand.NewSource(0)
@@ -56,4 +58,71 @@ func TestLogTextHandler(t *testing.T) {
 
 	s.End()
 	check(`time=? level=DEBUG msg="span ? finished in ?" span.name=level1 span.id=kYcTpgn span.parent=IvQORsV span.trace=bqzcRlJahlbbBZH span.dur=? span.source=?`)
+
+	tm, _ := time.Parse(time.RFC3339, "2013-05-13T19:30:00Z")
+	s, _ = Start(ctx, "custom", "started", tm)
+	check(`time=? level=DEBUG msg="span custom started" span.started=2013-05-13T19:30:00.000Z span.name=custom span.id=VIPEcES span.parent=IvQORsV span.trace=bqzcRlJahlbbBZH span.source=?`)
+
+	s.Event("one", "at", tm.Add(1*time.Minute))
+	if !strings.Contains(buf.String(), "span.at=1m0s") {
+		t.Errorf("buffer does not contain at: %s", buf.String())
+	}
+	check(`time=? level=DEBUG msg="span custom event one" span.started=2013-05-13T19:30:00.000Z span.at=? span.name=custom span.id=VIPEcES span.parent=IvQORsV span.trace=bqzcRlJahlbbBZH span.event=one span.at=? span.source=?`)
+
+	s.End("finished", tm.Add(10*time.Minute))
+	if !strings.Contains(buf.String(), "span.dur=10m0s") {
+		t.Errorf("buffer does not contain duration: %s", buf.String())
+	}
+	check(`time=? level=DEBUG msg="span ? finished in ?" span.started=2013-05-13T19:30:00.000Z span.finished=2013-05-13T19:40:00.000Z span.name=custom span.id=VIPEcES span.parent=IvQORsV span.trace=bqzcRlJahlbbBZH span.dur=? span.source=?`)
+}
+
+func TestFindArgsPairs(t *testing.T) {
+	one := 1
+	two := 2
+	tests := []struct {
+		args []any
+		key  string
+		want *int
+	}{
+		{[]any{}, "a", nil},
+		{[]any{"a"}, "a", nil},
+		{[]any{"a", 1}, "a", &one},
+		{[]any{"a", "1"}, "a", nil},
+		{[]any{"a", 1, "b", 2}, "b", &two},
+		{[]any{"a", 1, "b", 2}, "c", nil},
+		{[]any{"a", 1, "b"}, "b", nil},
+		{[]any{"a", 1, "b", 2, "a", 3}, "a", &one},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			got := findArgs[int](tt.args, tt.key)
+			if (got == nil) != (tt.want == nil) || (got != nil && *got != *tt.want) {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFindArgsAttrs(t *testing.T) {
+	tm, _ := time.Parse("2006-01-02T15:04:05Z, MST", "2013-05-13T19:30:00Z, CET")
+	tests := []struct {
+		args []any
+		key  string
+		want *time.Time
+	}{
+		{[]any{}, "a", nil},
+		{[]any{slog.String("x", "v")}, "a", nil},
+		{[]any{slog.String("a", "v")}, "a", nil},
+		{[]any{slog.Time("a", tm)}, "a", &tm},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			got := findArgs[time.Time](tt.args, tt.key)
+			if (got == nil) != (tt.want == nil) || (got != nil && *got != *tt.want) {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
